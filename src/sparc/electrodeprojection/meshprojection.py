b5a3f1dd6c928f64c042c2f37b5f40b57d9b7e22
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-11-25 14:30:54 +1300

    Improve PEP8 compiance in meshprojection.py.

--- src/sparc/electrodeprojection/meshprojection.py
+++ src/sparc/electrodeprojection/meshprojection.py
@@ -5,12 +5,11 @@ Created on 30 MOct, 2018 from mapclientplugins.meshgeneratorstep.
 """
 
 import numpy as np
-from opencmiss.zinc.field import Field
+
 from opencmiss.zinc.glyph import Glyph
-from opencmiss.zinc.graphics import Graphics
-from scaffoldmaker.utils.zinc_utils import *
 from opencmiss.zinc.node import Node
-from scaffoldmaker.scaffoldmaker import Scaffoldmaker
+from opencmiss.zinc.field import Field
+
 
 class MeshProjection(object):
 
@@ -18,21 +17,23 @@ class MeshProjection(object):
         self._region = region
         self._scene = self._region.getScene()
         # self._child_region = region.createChild('grid_plane')
-        self.startingNumber = 1000
-        self.globalCoords = []
-        self.xiCoords = {
+        self._starting_number = 1000
+        self._global_coords = []
+        self._plane_norm = None
+        self._xi_coordinates = {
             'element': [],
             'coordinates': [],
             'elementID': []
         }
 
-    def generateGridPoints4(self, pointsList, number_on_side, plane_normal):
+    def generate_grid_points_4(self, points_list, number_on_side, plane_normal):
         # We generate our grid points by having 4 points that we assign weightings to
         # based on how far we are away from them.
 
         # INPUTS:
         #   pointsList : a list of four points defined in any 3D space.
-        #   number_on_side : the number of nodes we have on each side of our array (ie 8 nodes if we have a 64 point grid).
+        #   number_on_side : the number of nodes we have on each side of our array
+        # (ie 8 nodes if we have a 64 point grid).
         #   plane_normal : the normal of the plane we are creating points in. EG [0,0,1] for the x,y plane.
 
         # OUTPUTS:
@@ -40,12 +41,13 @@ class MeshProjection(object):
         #       eg: [[1 , 0, 0], [1, .06, 0], [1, .12, 0], ... [0, 1, 0]]
 
         # Extra notes:
-        #   The points are selected in a counter clockwise fashion (start anywhere and make a trapezoid counterclockwise)
+        #   The points are selected in a counter clockwise fashion
+        # (start anywhere and make a trapezoid counterclockwise)
 
-        p1 = pointsList[0]
-        p2 = pointsList[1]
-        p3 = pointsList[2]
-        p4 = pointsList[3]
+        p1 = points_list[0]
+        p2 = points_list[1]
+        p3 = points_list[2]
+        p4 = points_list[3]
 
         ns1 = number_on_side - 1
 
@@ -76,42 +78,38 @@ class MeshProjection(object):
             shifted_point = grid_coord[i] + plane_norm * plane_normal_offset
             coord_list.append(shifted_point.tolist())
 
-
         return coord_list
 
-
-    def pointsToNodes(self, grid_list):
+    def points_to_nodes(self, grid_list):
 
         fm = self._region.getFieldmodule()
         cache = fm.createFieldcache()
         coordinates = fm.findFieldByName('coordinates')
         coordinates = coordinates.castFiniteElement()
-        colour = fm.findFieldByName('colour')
-        colour = colour.castFiniteElement()
         nodes = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
         nodetemplate = nodes.createNodetemplate()
         nodetemplate.defineField(coordinates)
         nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
 
         for i, grid_point in enumerate(grid_list):
-            eegNode = nodes.createNode(self.startingNumber + i, nodetemplate)
-            cache.setNode(eegNode)
+            eeg_node = nodes.createNode(self._starting_number + i, nodetemplate)
+            cache.setNode(eeg_node)
             coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, grid_point)
 
-    def renderNodes(self):
+    def render_nodes(self):
         self._scene.beginChange()
-        nodePoints = self._scene.createGraphicsPoints()
-        materialModule = self._scene.getMaterialmodule()
+        node_points = self._scene.createGraphicsPoints()
+        material_module = self._scene.getMaterialmodule()
         fm = self._region.getFieldmodule()
-        nodePoints.setFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        node_points.setFieldDomainType(Field.DOMAIN_TYPE_NODES)
         coordinates = fm.findFieldByName('coordinates')
         coordinates = coordinates.castFiniteElement()
-        nodePoints.setFieldDomainType(Field.DOMAIN_TYPE_NODES)
-        nodePoints.setCoordinateField(coordinates)
-        nodePoints.setMaterial(materialModule.findMaterialByName('blue'))
-        nodePoints.setVisibilityFlag(True)
+        node_points.setFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        node_points.setCoordinateField(coordinates)
+        node_points.setMaterial(material_module.findMaterialByName('blue'))
+        node_points.setVisibilityFlag(True)
 
-        nodePointAttr = nodePoints.getGraphicspointattributes()
+        nodePointAttr = node_points.getGraphicspointattributes()
         nodePointAttr.setGlyphShapeType(Glyph.SHAPE_TYPE_SPHERE)
         nodePointAttr.setBaseSize([.02, .02, .02])
         cmiss_number = fm.findFieldByName('cmiss_number')
@@ -119,32 +117,34 @@ class MeshProjection(object):
 
         self._scene.endChange()
 
-    def solveNodes(self):
+    def solve_nodes(self):
         fm = self._region.getFieldmodule()
         cache = fm.createFieldcache()
-        self.globalCoords = []
-        self.xiCoords = {
+        self._global_coords = []
+        self._xi_coordinates = {
             'element': [],
             'coordinates': [],
             'elementID': []
         }
         for i in range(0,64):
-            self.moveNode(self.startingNumber+ i , plane_normal=self._plane_norm,cache=cache)
+            self.moveNode(self._starting_number + i, plane_normal=self._plane_norm, cache=cache)
 
-    def moveNode(self,  nodeKey, plane_normal=[0, 1, 0], cache=None):
-        # moveNode uses dot products combined with opencmiss' evaluateMeshLocation function to solve where a point lies along a given normal (line in 3D).
+    def move_node(self, node_key, plane_normal=[0, 1, 0], cache=None):
+        # moveNode uses dot products combined with opencmiss' evaluateMeshLocation function
+        # to solve where a point lies along a given normal (line in 3D).
 
         # usage: Please not that this solver assumes that the user knows where the solution should be in one dimension,
         # for example: Where is the closest mesh point at x=0 (normal=[1,0,0]) starting at [1,3,2]?
 
         # Inputs:
         #   region: the region you wish to solve in (must contain a mesh and a the node you wish to move)
-        #   nodekey: identifier of the node we wish to project onto the mesh
+        #   node_key: identifier of the node we wish to project onto the mesh
         #   plane_normal: the direction we wish to project the node onto the mesh
         #   cache: (optional) pass the cache to this function to enhance performance
 
-        # Ouputs:
-        #   new_coords: the new coordinates of the node now that it is on the mesh (or if we could not solve it will be the last iteration)
+        # Outputs:
+        #   new_coordinates: the new coordinates of the node now that it is on the mesh
+        # (or if we could not solve it will be the last iteration)
 
         # Adjust the solving parameters here:
         max_iterations = 20
@@ -155,66 +155,70 @@ class MeshProjection(object):
         fm = self._region.getFieldmodule()
         coordinates = fm.findFieldByName('coordinates')
         coordinates = coordinates.castFiniteElement()
-        if cache == None:
+        if cache is None:
             cache = fm.createFieldcache()
 
         # Create templates
         nodes = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
-        nodetemplate = nodes.createNodetemplate()
-        nodetemplate.defineField(coordinates)
-        nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
+        node_template = nodes.createNodetemplate()
+        node_template.defineField(coordinates)
+        node_template.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
 
         # Create our first new node for the search
-        old_node = nodes.findNodeByIdentifier(nodeKey)
+        old_node = nodes.findNodeByIdentifier(node_key)
         cache.setNode(old_node)
-        [result, old_coords] = coordinates.evaluateReal(cache, 3)
+        [_, old_coordinates] = coordinates.evaluateReal(cache, 3)
 
         # Create our mesh search
         mesh = fm.findMeshByName('mesh3d')
-        mesh_location = fm.createFieldStoredMeshLocation(mesh)
         found_mesh_location = fm.createFieldFindMeshLocation(coordinates, coordinates, mesh)
         found_mesh_location.setSearchMode(found_mesh_location.SEARCH_MODE_NEAREST)
 
         # shift the point in preparation for first iteration
         plane_norm = np.array(plane_normal)
-        shifted_point = old_coords + plane_norm * plane_normal_offset
+        shifted_point = old_coordinates + plane_norm * plane_normal_offset
         coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, shifted_point.tolist())
 
         # initialise for our solver
-
-
-
         it = 1
-        old_coords = shifted_point
-        test_coords = [10, 10, 10]
-        new_coords = shifted_point
+        old_coordinates = shifted_point
+        test_coordinates = [10, 10, 10]
+        new_coordinates = shifted_point
+        el = None
 
-        while abs(np.linalg.norm(np.dot(test_coords, plane_norm) - np.dot(new_coords, plane_norm))) > tol:
-            # ^^ test if x and y changes are within tolerence by testing the magnitude of the difference of the dot products
-            #    in the direction of our normal
+        while abs(np.linalg.norm(np.dot(test_coordinates, plane_norm) - np.dot(new_coordinates, plane_norm))) > tol:
+            # ^^ test if x and y changes are within tolerance by testing the magnitude of the
+            # difference of the dot products in the direction of our normal
 
             # Find nearest mesh location using the evaluateMeshLocation function
-            [el, coords] = found_mesh_location.evaluateMeshLocation(cache, 3)
-            cache.setMeshLocation(el, coords)
-            [result, mesh_coords] = coordinates.evaluateReal(cache, 3)
+            [el, coordinates] = found_mesh_location.evaluateMeshLocation(cache, 3)
+            cache.setMeshLocation(el, coordinates)
+            [_, mesh_coordinates] = coordinates.evaluateReal(cache, 3)
 
-            # Update our search location by finding how far we have moved in the plane_normal directoin
-            new_coords = old_coords + np.dot(mesh_coords - old_coords, plane_norm) * plane_norm
+            # Update our search location by finding how far we have moved in the plane_normal direction
+            new_coordinates = old_coordinates + np.dot(mesh_coordinates - old_coordinates, plane_norm) * plane_norm
             cache.setNode(old_node)
-            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, new_coords.tolist())
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, new_coordinates.tolist())
 
             # switch our test coordinates
-            test_coords = old_coords
-            old_coords = new_coords
+            test_coordinates = old_coordinates
+            old_coordinates = new_coordinates
 
             # Break in case we can not converge
             it += 1
             if it > max_iterations:
-                print(f'Could not converge on node {nodeKey}')
+                print(f'Could not converge on node {node_key}')
                 break
 
-        print(f'Node {nodeKey} was solved in {it-1} iterations')
-        self.xiCoords['element'].append(el)
-        self.xiCoords['elementID'].append(el.getIdentifier())
-        self.xiCoords['coordinates'].append(coords)
-        self.globalCoords.append(new_coords)
+        print(f'Node {node_key} was solved in {it-1} iterations')
+        self._xi_coordinates['element'].append(el)
+        self._xi_coordinates['elementID'].append(el.getIdentifier())
+        self._xi_coordinates['coordinates'].append(coordinates)
+        self._global_coords.append(new_coordinates)
+
+
+MeshProjection.moveNode = MeshProjection.move_node
+MeshProjection.generateGridPoints4 = MeshProjection.generate_grid_points_4
+MeshProjection.pointsToNodes = MeshProjection.points_to_nodes
+MeshProjection.renderNodes = MeshProjection.render_nodes
+MeshProjection.solveNodes = MeshProjection.solve_nodes
